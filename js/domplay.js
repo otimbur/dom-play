30802b161634de55e0e1950e2471726a582f14db
otimbur <otimbur@sea.yearup.org>
2022-07-21 21:34:55 -0700

    dom edits

--- js/domplay.js
+++ js/domplay.js
@@ -7,24 +7,42 @@ for (let i = 0; i < actors.length; i++) {
     actors[i].style.backgroundColor = "yellow";
 } */
 
+// the function determines that we are ableto have the connection
 function myAlert() {
   alert("I'm clicked!");
 }
+// setting an array with the <span> on the the page
+let spans = document.querySelectorAll('#play span');
 
-let spans = document.querySelectorAll("#play span");
-
-console.log(spans);
-
+// loopinf throuth array, adding an event which represent the click on the page that
+// will highlight the line
 for (const mySpan of spans) {
-  mySpan.addEventListener("click", myAlert);
-  alert(mySpan.dataset.actor);
+  mySpan.addEventListener('click', function (ev) {
+    highlightActor(mySpan.dataset.actor);
+  });
+  //alert(mySpan.dataset.actor);
+}
+
+// this for loop is before the page loads
+// if the id=
+function highlightActor(actor) {
+  for (const mySpan of spans) {
+    if (actor == mySpan.dataset.actor) {
+      //current actor, highlihjt
+      mySpan.style.backgroundColor = 'black';
+    } else {
+      //not current actor, remove highlight
+      mySpan.style.backgroundColor = '';
+    }
+  }
+  //alert(mySpan.dataset.actor);
 }
 
 // el is an elais for the elemnt we are affecting. el is var that applies in the function.
 function highlight(el) {
-  if (el.style.backgroundColor == "yellow") {
-    el.style.backgroundColor = "white";
+  if (el.style.backgroundColor == 'black') {
+    el.style.backgroundColor = '';
   } else {
-    el.style.backgroundColor = "yellow";
+    el.style.backgroundColor = 'black';
   }
 }
